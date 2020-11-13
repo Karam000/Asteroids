4087a9613b4cd0f1409e7de7dc3fa806dbf48471
Mohamed Karam <mohamedkaram09@gmail.com>
2020-11-13 22:31:56 +0200

    Basic asteroid movement test

--- Asteroids/Assets/Scripts/Entities/Asteroids/Asteroid.cs
+++ Asteroids/Assets/Scripts/Entities/Asteroids/Asteroid.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     [SerializeField] AsteroidProbs_SO AsteroidProbs;
+    private GameObject asteroid;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+      
+    }
+    public void spawn(Vector2 position, Transform parent = null)
+    {
+        asteroid = Instantiate(this.gameObject, new Vector3(position.x,position.y,0),Quaternion.identity,parent);
+        rb = asteroid.GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.velocity = AsteroidProbs.Speed * direction;
+    }
 }
--- Asteroids/Assets/Scripts/Managers/GameManager.cs
+++ Asteroids/Assets/Scripts/Managers/GameManager.cs
@@ -7,16 +7,26 @@ public class GameManager : MonoBehaviour
     [SerializeField] Ship Ship;
     [SerializeField] Asteroid[] Asteroids;
     [SerializeField] Bullet Bullet;
+
     void Start()
     {
         //Instantiate(Ship.ShipPrefab.gameObject);
         //Instantiate(Asteroids[0].AsteroidPrefab.gameObject);
         //Instantiate(Bullet.BulletPrefab.gameObject);
-        Bullet.spawn();
+        Initialize();
     }
-
+    private void FixedUpdate()
+    {
+        Asteroids[0].Move(new Vector2(0, 1));
+    }
     void Update()
     {
-        
+
+    }
+
+    private void Initialize()
+    {
+        Vector2 pos = new Vector2(Random.Range(0, 5), Random.Range(0, 5));
+        Asteroids[0].spawn(pos);
     }
 }
